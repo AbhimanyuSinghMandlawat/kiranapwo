c94a798d616203ced0e6657b269a54a2104b2c31
AbhimanyuSinghMandlawat <am2886@srmist.edu.in>
2026-04-30 02:20:37 +0530

    newdeploy

--- kirana-pos-pwa/kirana-pos/src/services/stockAlerts.js
+++
@@ -1,7 +0,0 @@
-import { getAllStock } from "./db";
-
-export async function getStockAlerts() {
-  const stock = await getAllStock();
-
-  return stock.filter(item => item.quantity <= item.threshold);
-}
